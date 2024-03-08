af2dac395112c5c11f5622d45154c14201903591
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2024-03-08 12:40:44 +0530

    Learn Today Variable Declaration, Initialization, OverRide OR Update And Re-Declaration And Swap Two Variables
    Learn Today Variable Declaration, Initialization, OverRide OR Update And Re-Declaration And Swap Two Variable

---
+++ helloWorld.js
@@ -0,0 +1,9 @@
+// Camel Case: test.js, hello.js, helloWorld.js, myFirstProgram.js
+
+// Kebab: test.js, hello.js, hello-world.js, my-first-program.js
+
+// Snake: test.js, hello.js, hello_world.js, my_first_program.js
+
+var city; // variable Declaration
+city = "Pune"; // variable Initialization
+console.log(city);
