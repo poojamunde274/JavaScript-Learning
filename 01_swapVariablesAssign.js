d0672cfc3790b5adeb691863370b2041799f6dab
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2024-03-08 12:43:19 +0530

    Complete Assignment Of Declare And Initialize Variable And Swap Two Values And Three Numbers
    Complete Assignment Of Declare And Initialize Variable And Swap Two Values And Three Numbers

---
+++ 01_swapVariablesAssign.js
@@ -0,0 +1,22 @@
+console.log("--------------- Swap Two Values ---------------");
+
+var sweety ="Sweety";
+var cutie = "Cutie";
+console.log("Before Swap Values:", "Sweety's Value Is:", sweety, "Cutie's Value Is:", cutie);
+
+var temp = sweety;
+sweety = cutie;
+cutie = temp;
+console.log("After Swap Values:", "Sweety's Value Is:", sweety, "Cutie's Value Is:", cutie);
+console.log("--------------- Swap Three Numbers ---------------");
+
+var n1 = 100;
+var n2 = 200;
+var n3 = 300;
+console.log("Before Swap Variables:", "Num1:", n1, "Num2:", n2, "Num3:", n3);
+
+var temp = n1;
+n1 = n2;
+n2 = n3;
+n3 = temp;
+console.log("After Swap Variables:", "Num1:", n1, "Num2:", n2, "Num3:", n3);
---
+++ 01_variableAssign.js
@@ -0,0 +1,31 @@
+console.log("--------- Q.1 -----------");
+var myFullName;
+console.log("Before Initialization:",myFullName);
+
+myFullName = "Pooja Munde";
+console.log("My First And Last Name Only:",myFullName);
+
+myFullName ="Pooja Shahaji Munde";
+console.log("My First, Middle And Last Name:", myFullName);
+
+console.log("--------- Q.2 -----------");
+var myLove;
+myLove = "I Love Only JS";
+console.log("My Love Is:",myLove);
+
+console.log("--------- Q.3 -----------");
+var firstName = "Pooja";
+var lastName = "Munde";
+var age = 28;
+var address ="Pune";
+var pinCode = 411057;
+var gender = "Female";
+var isMarried = true;
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("My Age:", age);
+console.log("My Address:", address);
+console.log("My Pincode:", pinCode);
+console.log("Gender:", gender);
+console.log("Is Married:", isMarried);
