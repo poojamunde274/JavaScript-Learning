c77a094b11df464e2805c8c4bcdacbe932fd7531
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2024-03-12 12:08:16 +0530

    Complete String Assignment
    Complete String Assignment

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,15 @@
+
+function stringBasics(){
+    console.log(`My Dream Company is TCS.`);
+    console.log(`-----------------------------------------------------------------`);
+    var hobby1 = "Swimming";
+    var hobby2 = "Coding";
+    var hobby3 = "Traveling";
+    console.log(`My Hobbies Are: ${hobby1}, ${hobby2}, ${hobby3}`);
+    console.log(`My First Hobbies Length Is: ${hobby1.length}`);
+    console.log(`My Second Hobbies Length Is: ${hobby2.length}`);
+    console.log(`My Third Hobbies Length Is: ${hobby3.length}`);
+    var total = hobby1.length + hobby2.length + hobby3.length;
+    console.log(`Sum The Total Number Of Characters That Is Available In Hobbies Are: ${total} `);
+}
+stringBasics();
