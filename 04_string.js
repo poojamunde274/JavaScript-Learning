85a8a2582b1bdcf17b5413b691ef6e53d26ac358
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2024-03-13 10:15:08 +0530

    Learn Today String Method And String Template
    Learn Today String Method And String Template

--- 04_string.js
+++ 04_string.js
@@ -18,12 +18,90 @@ var lastChar = sentence.charAt(totalChars-1);
 console.log("Last char is:" , lastChar);
 
 console.log("========== indexOf() ==================");
+
 var greet = "Good Morning";
 var indexOfM = greet.indexOf('M');
 console.log("Index of char M is: ", indexOfM);
 
 console.log("========== concat() ==================");
+
 var firstName = "Gajanan";
 var lastName= " Kharat";
 var resultConcat = firstName.concat(lastName);
 console.log("Concat result is:", resultConcat);
+
+console.log("========== replace() ==================");
+
+var greet = "Good Morning";
+const afterReplace = greet.replace("Morning", "Afternoon");
+console.log(`After replace string is: ${afterReplace}`);
+
+console.log(`========== toUpperCase() ==================`);
+
+var greet = "Good Morning";
+var greetInUpperCase = greet.toUpperCase();
+console.log(`${greet} in upper case is : ${greetInUpperCase}`);
+
+console.log(`========== trim() ==================`);
+var greet = "   Good Morning   ";
+var lengthBeforeTrim = greet.length;
+console.log(` ${greet} it's length is : ${lengthBeforeTrim} `);
+
+var greetAfterTrim = greet.trim();
+var lengthAfterTrim = greetAfterTrim.length;
+console.log(` ${greetAfterTrim} it's length is : ${lengthAfterTrim} `);
+
+console.log(`Total spaces removed is: ${lengthBeforeTrim-lengthAfterTrim}`);
+
+// console.log(`========== include() ==================`);
+
+// var str = "My Name is codemind";
+// console.log(str.includes("Name"));
+
+console.log(`========== includes() ==================`);
+var greet = "Good Morning";
+var result = greet.includes("nin");
+console.log(`Is string ${greet} includes word or char 'nin': ${result}`);
+
+var result = greet.includes("MoR");
+console.log(`Is string ${greet} includes word or char 'MoR': ${result}`);
+
+// console.log(`========== Slice() ==================`);
+
+// var stringOne = "Developers World!"
+// console.log(stringOne.slice(2, 7));
+
+console.log(`========== slice() ==================`);
+var greet = "Good Morning";
+var result = greet.slice(3, 10);
+console.log(`Slice is ${result}`);
+
+console.log(`========== search() ==================`);
+var greet = "Good Morning";
+var result = greet.search("Morning");
+console.log(`'Morning' is available at ${result}`);
+
+var result = greet.search("r");
+console.log(`'r' is available at ${result}`);
+
+console.log(`========== split() ==================`);
+var greet = "Good Morning";
+var resultValue = greet.split(" ");
+console.log(resultValue);
+console.log(`Total words are: ${resultValue.length}`);
+
+// Write a function with name totalWord() with one arguments
+//  "I am happy Buddy" 
+//  "I am learning JS the language of internet"
+// and this function should return the total numbers of words
+
+function totalWord(sentence){
+    var words = sentence.split(" ");
+    var totalWords = words.length;
+    return totalWords
+}
+var totalWords = totalWord("I am happy Buddy");
+console.log(`"I am happy Buddy". Total number od words is: ${totalWords}`);
+
+var totalWords = totalWord("I am learning JS the language of internet");
+console.log(`"I am learning JS the language of internet". Total number od words is: ${totalWords}`);
