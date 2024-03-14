7a2702bb4279802fb22e89374c7b8e87cd85ff85
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2024-03-14 19:13:32 +0530

    Complete Operator's Assignment
    Complete Operator's Assignment

---
+++ 05_operator.js
@@ -0,0 +1,42 @@
+console.log(`----------------------------------- Q.1 ---------------------------------`);
+function squareOfWordLength(string){
+    console.log(`Word Is:'${string}'`);
+    var stringLength = string.length;
+    console.log(`Word Length is:${stringLength}`);
+
+    var squareOfLength = stringLength * stringLength;
+    return squareOfLength;
+
+}
+var squareOfLength = squareOfWordLength(`JavaScript`);
+console.log(`Square Of Length Is: ${squareOfLength}`);
+console.log(`--------------------------------------------------------------------`);
+
+var squareOfLength = squareOfWordLength(`Google Chrome`);
+console.log(`Square Of Length Is: ${squareOfLength}`);
+console.log(`--------------------------------------------------------------------`);
+
+var squareOfLength = squareOfWordLength(`Developer Smart`);
+console.log(`Square Of Length Is: ${squareOfLength}`);
+
+console.log(`--------------------------------- Q. 2 -----------------------------------`);
+
+function string(){
+    var str = "I Am Angular Developer";
+    var strLength = str.length;
+
+    var totalWord = str.split(" ");
+    var totalWordLength = totalWord.length;
+
+    console.log(`String Length Is:${strLength}`);
+    console.log(`Total Word Is: ${totalWordLength}`);
+
+    var divide = strLength / totalWordLength;
+    console.log(`Result Of Divided by String Length And its Total Word Length:${divide}`);
+    console.log(`--------------------------------------------------------------------`);
+
+    var multiple = strLength * totalWordLength
+    console.log(`Result Of Multiple by String Length And its Total Word Length:${multiple}`);
+
+}
+string();
