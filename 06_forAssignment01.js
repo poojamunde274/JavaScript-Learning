7c32a1c8999885e0fb76efb4dbde8f4993fa92cb
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2024-03-19 15:04:13 +0530

    Complete For Loop Assignment
    Complete For Loop Assignment

---
+++ 06_forAssignment01.js
@@ -0,0 +1,34 @@
+console.log(`WAP To Print Numbers From 5 To 15 By Incrementing 1.`);
+for (let index = 5; index <= 15; index++) {
+    console.log(index);  
+}
+
+console.log(`WAP To Print Numbers From 50 To 40 By Decrementing 1.`);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+console.log(`WAP To Find First 15 Odd Numbers.`);
+for (let index = 1; index <= 30; index=index+2) {
+    console.log(index);    
+}
+
+console.log(`WAP To Find First 10 Even Numbers.`);
+for (let index = 0; index < 20; index = index + 2) {
+    console.log(index);
+}
+
+console.log(`WAP To Print Table Of 5.`);
+for (let index = 5; index <=50; index = index + 5) {
+    console.log(index);
+}
+
+console.log(`WAP To Print Table Of 10.`);
+for (let index = 10; index <=100; index = index + 10) {
+    console.log(index);
+}
+
+console.log(`WAP To Print Table Of 10 In Reverse Order.`);
+for (let index = 100; index >=10; index = index - 10) {
+    console.log(index);
+}
